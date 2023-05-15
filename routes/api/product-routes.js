594d0c03c49304a222ea0f65fc9c1cbd114f8089
jkrieger6 <jkrieger11@gmail.com>
2023-05-15 10:43:43 -0700

    added route to create new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,6 +17,32 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create({
+  product_name: req.body.product_name,
+  price: req.body.price,
+  stock: req.body.stock,
+  tagIds: req.body.tagIds
+  })
+  .then((product) => {
+    // If there are product tags, create pairings to bulk create in the ProductTag model
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // If no product tags respond error
+    res.status(200).json(product);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -45,11 +71,10 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
-// update product
+// Update product
 router.put('/:id', (req, res) => {
-  // update product data
+  // Update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
