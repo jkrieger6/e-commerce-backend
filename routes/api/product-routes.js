2e49d3839dbd01151adf1b1c8702efc19e50f4fa
jkrieger6 <jkrieger11@gmail.com>
2023-05-17 16:02:45 -0700

    added delete to product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,8 +115,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id.' });
+      return;
+    }
+    res.json(productData);
+  }
+  catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
