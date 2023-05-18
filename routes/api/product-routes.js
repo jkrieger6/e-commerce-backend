396a69fb890bcde81363f533cf6ac0eda4925775
jkrieger6 <jkrieger11@gmail.com>
2023-05-17 17:38:13 -0700

    added end points for testing insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,21 +4,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/products', async (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
       const productData = await Product.findAll({
         include: [{ model: Category }, { model: Tag }],
       });
-      res.json(productData);
+      res.status(200).json(productData);
     } catch (err) {
       res.json(err);
     }
   });
 
 // get one product
-router.get('/:id', async (req, res) => {
+router.get('/products/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 try {
@@ -29,19 +29,19 @@ try {
       res.status(404).json({ message: 'No product found with this id.' });
       return;
     }
-    res.json(productData);
+    res.status(200).json(productData);
   } catch (err) {
-    res.json(err);
+    res.status(500).json(err);
   }
 });
   
 // create new product
-router.post('/', async (req, res) => {
+router.post('/products', async (req, res) => {
   try {
     const productData = await Product.create(req.body);
     res.status(200).json(productData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
   /* req.body should look like this...
@@ -70,11 +70,11 @@ router.post('/', async (req, res) => {
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 
 // Update product
-router.put('/:id', (req, res) => {
+router.put('/products/:id', (req, res) => {
   // Update product data
   Product.update(req.body, {
     where: {
@@ -111,11 +111,11 @@ router.put('/:id', (req, res) => {
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/products/:id', async (req, res) => {
   // delete one product by its `id` value
 try {
     const productData = await Product.destroy({
@@ -124,14 +124,14 @@ try {
       },
     });
     if (!productData) {
-      res.status(404).json({ message: 'No product found with this id.' });
+      res.status(500).json({ message: 'No product found with this id.' });
       return;
     }
-    res.json(productData);
+    res.json({ message: 'Product deleted successfully.' });
   }
   catch (err) {
     res.json(err);
-  }
+  } 
 });
 
 module.exports = router;
